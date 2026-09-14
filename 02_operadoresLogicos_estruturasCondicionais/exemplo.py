746094359bbe22c1471531dbd97d574f35a54790
Thiago Rafael Mathias <trthiago2009@gmail.com>
2026-09-14 07:49:08 -0300

    feat: exemplo.py file added in the second class folder

---
+++ 02_operadoresLogicos_estruturasCondicionais/exemplo.py
@@ -0,0 +1,9 @@
+nome = "Thigas"
+idade = 19
+
+if(nome == "Thigas" and idade < 18 and idade > 0):
+    print("Thigas é menor de idade!")
+elif(idade == 18):
+    print("Thigas tem 18 anos de idade!")
+else:
+    print("Thigas tem mais de 18 anos de idade!")
